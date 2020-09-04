f8641557b5d566a4a5d6ac8e9436400c0c53290b
MDfarazuddin <34812095+MDfarazuddin@users.noreply.github.com>
2020-09-04 11:49:13 +0530

    can save to csv for nested elements

---
+++ main.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+tree = ET.parse('OnDemandBillRun-9910995780.xml')
+
+root = tree.getroot()
+
+for element in root.iter():
+    print(element.tag,',',element.attrib,'=',element.text)
+
+path = input()
+a = root.findall(path)
+
+csv = dict()
+
+for index,child in enumerate(a):
+    print('\t|-->',index,child.tag,',',child.attrib,'=',child.text)
+    csv[child.tag] = child.text
+
+print(csv)
+csv = pd.DataFrame(csv,index=[0])
+file_name = input()
+csv.to_csv('{}.csv'.format(file_name))
