c6169550502fedaf87f90c1a0bc024a72500f626
MDfarazuddin <34812095+MDfarazuddin@users.noreply.github.com>
2020-09-07 11:26:47 +0530

    Xpath changes made

--- interface.py
+++ interface.py
@@ -2,17 +2,14 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 
-def load_interface(file_path):
-    tree = ET.parse(file_path)
-    root = ET.getroor(tree)
-    return ET.tag
-
-
 if __name__ == '__main__':
-    # print(load_interface('interface.xml'))
-    tree = ET.parse('interface.xml')
+    tree = ET.parse('column1.xml')
     root = tree.getroot()
     print('Root tag is ',root.tag)
-    for index,child in enumerate(root.iter()):
-        if  child.attrib != {}:
-            print(index,child.tag,',',child.attrib,'=',child.text)
+    # for index,child in enumerate(root.iter()):
+    #     #  only root nodes
+    #     if  len(child) == 0:
+    #         print(index,child.tag,',',child.attrib,'=',child.text)
+    target_xpath = './InvHeader/Totals/MonAmnt[@Type="TaxAmount"]'
+    target_tag = root.find(target_xpath)
+    print(target_tag.tag,target_tag.attrib,target_tag.text)
--- main.py
+++ main.py
@@ -1,26 +1,8 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
-tree = ET.parse('OnDemandBillRun-9910995780.xml')
+tree = ET.parse('./Bills/OnDemandBillRun-9910995780.xml')
 
 root = tree.getroot()
-
-for element in root.iter():
-    print(element.tag,',',element.attrib,'=',element.text)
-# for element in root.iter():
-#     print(element.tag,',',element.attrib,'=',element.text)
-
-path = input()
-a = root.findall(path)
-
-csv = dict()
-
-for index,child in enumerate(a):
-    print('\t|-->',index,child.tag,',',child.attrib,'=',child.text)
-    csv[child.tag] = child.text
-
-print(csv)
-csv = pd.DataFrame(csv,index=[0])
-file_name = input()
-csv.to_csv('{}.csv'.format(file_name))
-for child in root.iter('MonAmnt'):
-    print(child.tag,child.attrib,child.tag)
+totals_tag = root.find('Invoice')
+for child in totals_tag.iter():
+    print(child.tag,child.attrib,child.text)
