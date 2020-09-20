5356ee9a92a1b64361fa513b43b8c2a94541a16f
MDfarazuddin <34812095+MDfarazuddin@users.noreply.github.com>
2020-09-20 21:07:57 +0530

    removed main coz it was useless

--- main.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-import pandas as pd
-tree = ET.parse('./Bills/OnDemandBillRun-9910995780.xml')
-
-root = tree.getroot()
-totals_tag = root.find('Invoice')
-for child in totals_tag.iter():
-    print(child.tag,child.attrib,child.text)
